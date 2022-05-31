fe147864d7e3d7df6d609e224e6c09b9378e61d8
Kunal Verma <vkunal321@gmail.com>
2022-06-01 01:29:11 +0530

    Update project

--- 3-months Prep/Leetcode/Practice/src/Main.java
+++ 3-months Prep/Leetcode/Practice/src/Main.java
@@ -1,7 +1,5 @@
-package com.kunal;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world!");
     }
 }
